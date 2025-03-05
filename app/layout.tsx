55c2c7d0261690c4358fa8bdfb82580fe7fd334c
ChevvyChelios <souravsarania2@gmail.com>
2025-03-05 23:49:04 +0530

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="container">
+          <div className="wrapper">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
